529a2fd37d5970bd94f2429d8adc2209157fdd04
Deepak1230987 <singhdeepak@kgpian.iitkgp.ac.in>
2025-07-08 17:28:10 +0530

    refactor: remove unused 'Remember me' and 'Forgot password' options from login form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -182,31 +182,7 @@ const Login = () => {
                 </div>
               </div>
 
-              <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <input
-                    id="remember_me"
-                    name="remember_me"
-                    type="checkbox"
-                    className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
-                  />
-                  <label
-                    htmlFor="remember_me"
-                    className="ml-2 block text-sm text-gray-700"
-                  >
-                    Remember me
-                  </label>
-                </div>
-
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-medium text-blue-600 hover:text-blue-500"
-                  >
-                    Forgot password?
-                  </a>
-                </div>
-              </div>
+        
 
               {/* SimpleCaptcha Component */}
               <div className="mt-4">
